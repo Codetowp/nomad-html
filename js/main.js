c7a48a0402686353097cf40d4a2cea15175ade1f
Rijo Abraham <30547872+rijoabraham@users.noreply.github.com>
2017-09-22 16:58:42 +0530

    update 22/9/17

--- js/main.js
+++ js/main.js
@@ -87,6 +87,7 @@ function main() {
         offset: 10
     })
 
+
   	
   
 
@@ -137,82 +138,40 @@ $(window).resize();
 /*creating click event*/
 $(document).ready(function(){
   
-   $("#clients").owlCarousel({
+   $("#article-slider").owlCarousel({
   	 
-  	      navigation : false, // Show next and prev buttons
+  	      navigation : true, // Show next and prev buttons
   	      slideSpeed : 300,
   	      paginationSpeed : 400,
   	      autoHeight : true,
+		  navigationText : ["", ""],
+		  singleItem:true,
   	      itemsCustom : [
 				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
+				        [450, 1],
+				        [600, 1],
+				        [700, 1],
+				        [1000, 1],
+				        [1200, 1],
+				        [1400, 1],
+				        [1600, 1]
 				      ],
   	  });
 
   
+
   
-  
-  $("#testimonial").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true,
-		afterInit: makePages,
-        afterUpdate: makePages
-        });
+
 		
 		
 		
-		 function makePages() {
-        $.each(this.owl.userItems, function(i) {
-            $('#testimonial .owl-controls .owl-page').eq(i)
-                .css({
-                    'background': 'url(' + $(this).find('img').attr('src') + ')',
-                    'background-size': 'cover'
-                })
-        });
-    }
+    
   
 });
 
 
 
 
-  	/*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-        var $container = $('#lightbox');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
 
 
 
